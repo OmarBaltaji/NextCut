c14298db550c1ec7cc26310903b1d6703cd2bfe7
OmarBaltaji <74295131+OmarBaltaji@users.noreply.github.com>
2021-02-02 23:50:55 +0200

    Update firebase-messaging-sw.js

--- NextCut/public/firebase-messaging-sw.js
+++ NextCut/public/firebase-messaging-sw.js
@@ -2,14 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/6.6.2/firebase-messaging.js');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyClC2KR_u0spVxP3o5GYmWx4KaPozTu0ew",
-    authDomain: "nextcut-636e9.firebaseapp.com",
-    databaseURL: "https://nextcut-636e9-default-rtdb.firebaseio.com",
-    projectId: "nextcut-636e9",
-    storageBucket: "nextcut-636e9.appspot.com",
-    messagingSenderId: "306622715186",
-    appId: "1:306622715186:web:9ca034409897fdeae8f32b",
-    measurementId: "G-0FHXHRF2NV"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
